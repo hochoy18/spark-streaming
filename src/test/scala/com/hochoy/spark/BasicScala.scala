a7d0263cd4f0f4b11eb46355292cd0ca2b642613
cai.he <cai.he@wbkit.com>
2019-01-14 20:17:27 +0800

    scala option

--- src/test/scala/com/hochoy/spark/BasicScala.scala
+++ src/test/scala/com/hochoy/spark/BasicScala.scala
@@ -3,7 +3,10 @@ package com.hochoy.spark
 import org.scalatest.FunSuite
 import com.hochoy.spark.hadoop._
 
-/** Describe:
+import scala.util.{Success, Try}
+
+
+/** Describe:https://windor.gitbooks.io/beginners-guide-to-scala/content/chp5-the-option-type.html
   * Created by IntelliJ IDEA.
   * Time: 16:34
   *
@@ -17,10 +20,46 @@ class BasicScala extends FunSuite {
     println(s"key: ${app.key}  , value:  ${app.value},  ${app}")
 
     val app2 = applyDemo("123", "12345")
-    println(app2.key,app2.value)
+    println(app2.key, app2.value)
   }
 
   test("ip location") {
   }
 
+
+  test("option test") {
+
+  }
+
+}
+
+case class User(val id: Int, val fName: String,val lName: String, val age: Int, val gender: Boolean)
+
+object UserRepository {
+  val users = Map(1 → User(1, "1", "1", 1, true), 2 → User(2, "2", "2", 2, false))
+
+  def findById(id: Int): Option[User] = {
+    users.get(id)
+  }
+  def findFNameMatch(id:Int):User={
+    val v = users.get(id)
+    v  match {
+      case Some(v) ⇒ v
+      case None ⇒ null
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val user = User(99999, "Johanna", "Doe", 30, true)
+    println(Try(10 / 0))
+    println("....  ",findFNameMatch(2))
+
+
+    println(findById(1).getOrElse(user))
+    println(findById(1).isDefined)
+    println(findById(2).getOrElse(user))
+    println(findById(3).getOrElse(user))
+    println(findById(3).isDefined)
+    println(findById(1).nonEmpty)
+  }
 }
